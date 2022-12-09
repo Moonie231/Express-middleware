40336c201172b77cd0d4962eb4741875a9d0d1fb
Moonie231 <haizzzhmmm@gmail.com>
2022-12-09 14:06:34 +0700

    middleware helmet

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const morgan_1 = __importDefault(require("morgan"));
+const helmet_1 = __importDefault(require("helmet"));
 const app = (0, express_1.default)();
+app.use((0, helmet_1.default)());
 app.use(body_parser_1.default.json());
 app.use((0, morgan_1.default)('common'));
 app.get('/', (req, res) => {
--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from "body-parser";
-import morgan from "morgan"
+import morgan from "morgan";
+import helmet from "helmet";
 
 const app = express();
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(morgan('common'))
